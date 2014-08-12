bbf2983bb78fa773fd4c3896b4388fee7e1e9bd5
foreveross－bj <Developer@foreverossbjdeMacBook-Pro.local>
2014-08-12 09:34:20 +0800

    Initial Commit

---
+++ ImagePicker/ZLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZLAppDelegate.h
+//  ImagePicker
+//
+//  Created by foreveross－bj on 14-8-12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImagePicker
+//
+//  Created by foreveross－bj on 14-8-12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZLAppDelegate class]));
+    }
+}
